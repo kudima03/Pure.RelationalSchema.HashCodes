ff8202cd64860979e0b1f89a9ec9a36d0929562b
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-23 16:48:23 +0300

    Implemented ColumnTypeHash.

---
+++ src/Pure.RelationalSchema.HashCodes/ColumnTypeHash.cs
@@ -0,0 +1,39 @@
+﻿using Pure.HashCodes;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using System.Collections;
+
+namespace Pure.RelationalSchema.HashCodes;
+public sealed record ColumnTypeHash : IDeterminedHash
+{
+    private static readonly byte[] TypePrefix =
+    [
+        8, 157, 151, 1, 149, 98, 28, 119, 130, 158, 187, 34, 130, 255, 222, 135
+    ];
+
+    private readonly IColumnType _columnType;
+
+    public ColumnTypeHash(IColumnType columnType)
+    {
+        _columnType = columnType;
+    }
+
+    public IEnumerator<byte> GetEnumerator()
+    {
+        return new DeterminedHash(TypePrefix.Concat(new DeterminedHash(_columnType.Name))).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
