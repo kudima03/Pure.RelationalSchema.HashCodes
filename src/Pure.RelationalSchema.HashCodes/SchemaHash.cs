c612f1a3c9ef1bbc5e3e555534a1333b82bb58fa
Michww3 <michww3@gmail.com>
2026-03-13 11:38:11 +0300

    (SchemaHash) Formatting

--- src/Pure.RelationalSchema.HashCodes/SchemaHash.cs
+++ src/Pure.RelationalSchema.HashCodes/SchemaHash.cs
@@ -39,56 +39,63 @@ public sealed record SchemaHash : IDeterminedHash
             schema.Name,
             schema.Tables,
             schema.ForeignKeys
-            )
+        )
     { }
 
     public SchemaHash(IString name, IEnumerable<ITable> tables, IEnumerable<IForeignKey> foreignKeys)
         : this(
               new DeterminedHash(name),
               tables,
-              foreignKeys)
+              foreignKeys
+        )
     { }
 
     public SchemaHash(IDeterminedHash nameHash, IEnumerable<ITable> tables, IEnumerable<IForeignKey> foreignKeys)
         : this(
               nameHash,
               new DeterminedHash(tables.Select(t => new TableHash(t))),
-              foreignKeys)
+              foreignKeys
+        )
     { }
 
     public SchemaHash(IString name, IDeterminedHash tablesHash, IEnumerable<IForeignKey> foreignKeys)
         : this(
               name,
               tablesHash,
-              new DeterminedHash(foreignKeys.Select(fk => new ForeignKeyHash(fk))))
+              new DeterminedHash(foreignKeys.Select(fk => new ForeignKeyHash(fk)))
+        )
     { }
 
     public SchemaHash(IString name, IEnumerable<ITable> tables, IDeterminedHash foreignKeysHash)
         : this(
               new DeterminedHash(name),
               tables,
-              foreignKeysHash)
+              foreignKeysHash
+        )
     { }
 
     public SchemaHash(IDeterminedHash nameHash, IDeterminedHash tablesHash, IEnumerable<IForeignKey> foreignKeys)
         : this(
               nameHash,
               tablesHash,
-              new DeterminedHash(foreignKeys.Select(fk => new ForeignKeyHash(fk))))
+              new DeterminedHash(foreignKeys.Select(fk => new ForeignKeyHash(fk)))
+        )
     { }
 
     public SchemaHash(IDeterminedHash nameHash, IEnumerable<ITable> tables, IDeterminedHash foreignKeysHash)
         : this(
               nameHash,
               new DeterminedHash(tables.Select(t => new TableHash(t))),
-              foreignKeysHash)
+              foreignKeysHash
+        )
     { }
 
     public SchemaHash(IString name, IDeterminedHash tablesHash, IDeterminedHash foreignKeysHash)
         : this(
               new DeterminedHash(name),
               tablesHash,
-              foreignKeysHash)
+              foreignKeysHash
+        )
     { }
 
     public SchemaHash(IDeterminedHash nameHash, IDeterminedHash tablesHash, IDeterminedHash foreignKeysHash)
